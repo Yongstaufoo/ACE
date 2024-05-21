3cdda1a89db2044d3f4617835a5a3f16e942c8f7
Yongstaufoo <myremember@gmail.com>
2024-05-21 09:03:03 +0000

    directToPage

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function showAlert() {
+function showAlertAndRedirect(action) {
     alert("NOTE: The use of this web application is in a personal capacity only.");
+    let url;
+    switch (action) {
+        case 'buy':
+            url = 'https://example.com/buy';
+            break;
+        case 'sell':
+            url = 'https://example.com/sell';
+            break;
+        case 'loan':
+            url = 'https://example.com/loan';
+            break;
+        default:
+            url = '#';
+    }
+    window.location.href = url;
 }
