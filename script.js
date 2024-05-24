4ca472b33fb846bb68cadb2baebed6a69d0c9908
Yongstaufoo <myremember@gmail.com>
2024-05-24 00:21:44 +0000

    sellAndBorrow

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 function showAlertAndRedirect(action) {
-    alert("NOTE: The use of this web application is in a personal capacity only.");
+    alert("NOTE: To prevent scams, ONLY communicate with official user's email.");
     let url;
     switch (action) {
         case 'buy':
             url = 'buy.html';
             break;
         case 'sell':
-            url = 'https://example.com/sell';
+            url = 'sell.html';
             break;
         case 'loan':
-            url = 'https://example.com/borrow';
+            url = 'borrow.html';
             break;
         default:
             url = '#';
     }
     window.location.href = url;
 }
+document.addEventListener("DOMContentLoaded", function() {
+    const description = document.getElementById('description');
+    const charCount = document.getElementById('char-count');
+    const maxChars = 200;
+
+    description.addEventListener('input', function() {
+        const remaining = maxChars - description.value.length;
+        charCount.textContent = `${remaining} characters remaining`;
+    });
+});
